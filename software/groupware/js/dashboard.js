33151cdd9d8d18a5f7ab0d6bda5bcef93d8b9707
purampurushotham <puram.purushotham@india.semanticbits.com>
2016-12-27 12:49:47 +0530

    Updated all files (json css dashboard.js) for dashboard files

--- software/groupware/js/dashboard.js
+++ software/groupware/js/dashboard.js
@@ -5,29 +5,7 @@
 
  */
 function imageClick(url) {
+//    alert("hai");
     window.location = url;
-}
 
-function chartRender(){
-    var canvas = document.getElementById("can");
-    var ctx = canvas.getContext("2d");
-    var lastend = 0;
-    var data = [200, 60, 15]; // If you add more data values make sure you add more colors
-    var myTotal = 0; // Automatically calculated so don't touch
-    var myColor = ['red', 'green', 'blue']; // Colors of each slice
-
-    for (var e = 0; e < data.length; e++) {
-        myTotal += data[e];
-    }
-
-    for (var i = 0; i < data.length; i++) {
-        ctx.fillStyle = myColor[i];
-        ctx.beginPath();
-        ctx.moveTo(canvas.width / 2, canvas.height / 2);
-        // Arc Parameters: x, y, radius, startingAngle (radians), endingAngle (radians), antiClockwise (boolean)
-        ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height / 2, lastend, lastend + (Math.PI * 2 * (data[i] / myTotal)), false);
-        ctx.lineTo(canvas.width / 2, canvas.height / 2);
-        ctx.fill();
-        lastend += Math.PI * 2 * (data[i] / myTotal);
-    }
 }
